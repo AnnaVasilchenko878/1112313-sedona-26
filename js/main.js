32644d9c7eac5a4ed973ab94a3fe70dd098afd02
AnnaVasilchenko878 <54268958+AnnaVasilchenko878@users.noreply.github.com>
2019-10-16 22:00:16 +0300

    Домашнее задание готово к проверке

--- js/main.js
+++ js/main.js
@@ -23,3 +23,15 @@ function init() {
     myMap.geoObjects
         .add(myGeoObject)
 }
+var modalButton = document.querySelector('.button-search-hotel');
+var modal = document.querySelector('.hotel-search-modal');
+var textOpacity = document.querySelector('.form-inputs');
+modalButton.addEventListener('click', function () {
+  if (!modal.classList.contains('modal-close') && !textOpacity.classList.contains('content-opacity')) {
+    modal.classList.add('modal-close');
+    textOpacity.classList.add('content-opacity');
+  } else {
+    modal.classList.remove('modal-close');
+    textOpacity.classList.remove('content-opacity');
+  }
+});
